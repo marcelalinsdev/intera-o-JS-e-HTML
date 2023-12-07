d1a214154cc7c32af4d6cb7a434ca266e6c01c36
Marcela <marceladslins@icloud.com>
2023-12-07 13:18:51 -0300

    adição de novos arquivos

---
+++ main.js
@@ -0,0 +1,32 @@
+    const form = document.getElementById('form-deposito')
+
+    function validaNome(nomeCompleto){
+        const nomeComoArray = nomeCompleto.split(' ');
+        return nomeComoArray.length >= 2;
+    }
+
+    form.addEventListener('submit', function(e) {
+    let formValido = false;
+    e.preventDefault();
+
+    const nomeBeneficiario = document.getElementById('nome-beneficiario');
+    const numeroContaBeneficiario = document.getElementById('numero-conta');
+    const valorDeposito = document.getElementById('valor-deposito');
+    const mensagemSucesso = `Montante de ${valorDeposito.value} depositado para o cliente: ${nomeBeneficiario.value} - conta ${numeroContaBeneficiario.value}`;
+
+    formValido = validaNome(nomeBeneficiario.value)
+    if (formValido) {
+        alert(mensagemSucesso)
+        nomeBeneficiario.value = '';
+        numeroContaBeneficiario.value = '';
+        valorDeposito.value = '';
+
+
+
+        } else {
+            alert("o nome não está completo");
+        }
+    })
+
+    console.log(form);
+        
